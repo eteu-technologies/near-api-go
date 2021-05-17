8f883c3fbd3e425616aeb4b9c942e1e81a18d9bb
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-17 16:15:18 +0300

    Start implementing ExecutionStatus

--- types/basic.go
+++ types/basic.go
@@ -38,9 +38,3 @@ func (bal Balance) String() string {
 
 // Nonce for transactions.
 type Nonce uint64
-
-// ExecutionStatus
-// - Unknown; The execution is pending or unknown.
-// - Failure(TxExecutionError); The execution has failed with the given execution error.
-// - SuccessValue([]byte); The final action succeeded and returned some value or an empty vec.
-// - SucessReceiptId(CryptoHash); The final action of the receipt returned a promise or the signed transaction was converted to a receipt. Contains the receipt_id of the generated receipt.
