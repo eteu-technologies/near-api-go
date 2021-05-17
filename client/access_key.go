a5b1c9607482f0d27a0f750f185501337df14085
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-17 22:01:27 +0300

    Start implementing NEAR RPC client

---
+++ client/access_key.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"context"
+
+	"github.com/eteu-technologies/near-api-go/jsonrpc"
+	"github.com/eteu-technologies/near-api-go/types/key"
+)
+
+// TODO: decode response
+// https://docs.near.org/docs/develop/front-end/rpc#view-access-key
+func (c *Client) AccessKeyView(ctx context.Context, accountID string, publicKey key.Base58PublicKey, block BlockCharacteristic) (res jsonrpc.JSONRPCResponse, err error) {
+	res, err = c.doRPC(ctx, "query", block, map[string]interface{}{
+		"request_type": "view_access_key",
+		"account_id":   accountID,
+		"public_key":   publicKey,
+	})
+
+	return
+}
+
+// TODO: decode response
+// https://docs.near.org/docs/develop/front-end/rpc#view-access-key-list
+func (c *Client) AccessKeyViewList(ctx context.Context, accountID string, block BlockCharacteristic) (res jsonrpc.JSONRPCResponse, err error) {
+	res, err = c.doRPC(ctx, "query", block, map[string]interface{}{
+		"request_type": "view_access_key_list",
+		"account_id":   accountID,
+	})
+
+	return
+}
+
+// TODO: decode response
+// https://docs.near.org/docs/develop/front-end/rpc#view-access-key-changes-single
+func (c *Client) AccessKeyViewChanges(ctx context.Context, accountID string, publicKey key.Base58PublicKey, block BlockCharacteristic) (res jsonrpc.JSONRPCResponse, err error) {
+	res, err = c.doRPC(ctx, "EXPERIMENTAL_changes", block, map[string]interface{}{
+		"changes_type": "single_access_key_changes",
+		"keys": map[string]interface{}{
+			"account_id": accountID,
+			"public_key": publicKey,
+		},
+	})
+
+	return
+}
+
+// TODO: decode response
+// https://docs.near.org/docs/develop/front-end/rpc#view-access-key-changes-all
+func (c *Client) AccessKeyViewChangesAll(ctx context.Context, accountIDs []string, block BlockCharacteristic) (res jsonrpc.JSONRPCResponse, err error) {
+	res, err = c.doRPC(ctx, "EXPERIMENTAL_changes", block, map[string]interface{}{
+		"changes_type": "all_access_key_changes",
+		"account_ids":  accountIDs,
+	})
+
+	return
+}
---
+++ client/client.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"context"
+
+	"github.com/eteu-technologies/near-api-go/jsonrpc"
+)
+
+type Client struct {
+	RPCClient jsonrpc.JSONRPCClient
+}
+
+func NewClient(networkAddr string) (client Client, err error) {
+	client.RPCClient, err = jsonrpc.NewClient(networkAddr)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (c *Client) doRPC(ctx context.Context, method string, block BlockCharacteristic, params interface{}) (res jsonrpc.JSONRPCResponse, err error) {
+	if block != nil {
+		if mapv, ok := params.(map[string]interface{}); ok {
+			block(mapv)
+		}
+	}
+
+	res, err = c.RPCClient.CallRPC(ctx, method, params)
+	return
+}
