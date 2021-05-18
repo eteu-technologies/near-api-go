6d87f0f75ffb5ec4558bbee04d73c616b140c1ef
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-18 19:23:44 +0300

    Turn basic types into aliases, instead of wrappers

--- types/basic.go
+++ types/basic.go
@@ -1,13 +1,13 @@
 package types
 
 // Account identifier. Provides access to user's state.
-type AccountID string
+type AccountID = string
 
 // Gas is a type for storing amounts of gas.
-type Gas uint64
+type Gas = uint64
 
 // Nonce for transactions.
-type Nonce uint64
+type Nonce = uint64
 
 // Time nanoseconds fit into uint128. Using existing Balance type which
 // implements JSON marshal/unmarshal
