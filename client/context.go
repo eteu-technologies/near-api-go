a421114ef3c23c56c73be082741eb80f2fa9fe36
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-18 19:43:16 +0300

    Create easy transaction wrapper

---
+++ client/context.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"context"
+
+	"github.com/eteu-technologies/near-api-go/types/key"
+)
+
+type rpcContext int
+
+const (
+	clientCtx = rpcContext(iota)
+	keyPairCtx
+)
+
+func ContextWithKeyPair(ctx context.Context, keyPair key.KeyPair) context.Context {
+	kp := keyPair
+	return context.WithValue(ctx, keyPairCtx, &kp)
+}
+
+func getKeyPair(ctx context.Context) *key.KeyPair {
+	return ctx.Value(keyPairCtx).(*key.KeyPair)
+}
