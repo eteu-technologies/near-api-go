a5e9890851f4f43911af7111a764cbc447d76284
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-12 14:38:16 +0200

    Add Nix flake dev env + direnv

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "near-api-go development shell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.go
+            pkgs.golangci-lint
+            pkgs.gopls
+            pkgs.act
+          ];
+        };
+      });
+}
