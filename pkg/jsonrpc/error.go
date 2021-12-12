ad954aaf1a1fa135f81d165e5d068cff032bd019
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-12 14:51:08 +0200

    Initial attemt at implementing structurized errors decoding

--- pkg/jsonrpc/error.go
+++ pkg/jsonrpc/error.go
@@ -3,6 +3,7 @@ package jsonrpc
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 )
 
 const (
@@ -17,11 +18,66 @@ const (
 )
 
 type Error struct {
+	Name  string     `json:"name"`
+	Cause ErrorCause `json:"cause"`
+
+	// Legacy - do not rely on them
 	Code    int             `json:"code"`
 	Message string          `json:"message"`
 	Data    json.RawMessage `json:"data"`
 }
 
-func (err Error) Error() string {
-	return fmt.Sprintf("JSON-RPC error '%s' (%d) %s", err.Message, err.Code, string(err.Data))
+func (err *Error) Error() string {
+	return fmt.Sprintf("RPC error %s (%s)", err.Name, err.Cause.String())
+}
+
+type ErrorCause struct {
+	Name string          `json:"name"`
+	Info json.RawMessage `json:"info"`
+
+	message *ErrorCauseMessage
+}
+
+type ErrorCauseMessage struct {
+	ErrorMessage string `json:"error_message"`
+}
+
+func (cause *ErrorCause) UnmarshalJSON(b []byte) (err error) {
+	var data struct {
+		Name string          `json:"name"`
+		Info json.RawMessage `json:"info"`
+	}
+
+	if err = json.Unmarshal(b, &data); err != nil {
+		err = fmt.Errorf("unable to unmarshal error cause: %w", err)
+		return
+	}
+
+	var info map[string]interface{}
+	if err = json.Unmarshal(data.Info, &info); err != nil {
+		err = fmt.Errorf("unable to unmarshal error cause info: %w", err)
+		return
+	}
+
+	var message *ErrorCauseMessage
+	if v, ok := info["error_message"]; ok {
+		message = &ErrorCauseMessage{
+			ErrorMessage: v.(string),
+		}
+	}
+
+	*cause = ErrorCause{
+		Name:    data.Name,
+		Info:    data.Info,
+		message: message,
+	}
+
+	return
+}
+
+func (cause ErrorCause) String() string {
+	if cause.message != nil {
+		return fmt.Sprintf("name=%s, message=%s", cause.Name, cause.message.ErrorMessage)
+	}
+	return fmt.Sprintf("name=%s, info=%s", cause.Name, strconv.Quote(string(cause.Info)))
 }
