1e87333dc8e710d97df8b523191a71f7e037fed3
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-13 14:02:53 +0200

    Handle nil body

--- pkg/jsonrpc/client.go
+++ pkg/jsonrpc/client.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"sync/atomic"
@@ -59,11 +59,20 @@ func (c *Client) CallRPC(ctx context.Context, method string, params interface{})
 		return
 	}
 
-	return parseRPCBody(response.Body)
+	return parseRPCBody(response)
 }
 
-func parseRPCBody(body io.ReadCloser) (res Response, err error) {
+func parseRPCBody(r *http.Response) (res Response, err error) {
+	//fmt.Printf("%#v\n", r)
+
+	body := r.Body
+	if body == nil {
+		err = errors.New("nil body")
+		return
+	}
 	defer func() { _ = body.Close() }()
+
+	// TODO: check for Content-Type header
 	decoder := json.NewDecoder(body)
 	decoder.DisallowUnknownFields()
 
