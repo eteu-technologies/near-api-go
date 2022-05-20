662f4224b88e82650b074d3738952236d8f092fa
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-05-20 15:58:50 +0300

    Clean up old files

--- nearnet/get_creds.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-datadir="$(docker volume inspect nearnet_data --format '{{ .Mountpoint }}')"
-sudo cat "${datadir}"/localnet/node0/validator_key.json
--- nearnet/tail_logs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-docker-compose exec nearup tail -f /root/.nearup/logs/localnet/node0.log
