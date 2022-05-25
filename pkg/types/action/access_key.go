c042eac16f2136a9b6b464ecb63817e9e2463a25
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-05-25 13:27:35 +0300

    Fix AccessKeyPermission JSON decoding, fix FullAccessKey borsh enum value

--- pkg/types/action/access_key.go
+++ pkg/types/action/access_key.go
@@ -1,6 +1,9 @@
 package action
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/eteu-technologies/borsh-go"
 	"github.com/eteu-technologies/near-api-go/pkg/types"
 )
@@ -10,6 +13,10 @@ type AccessKeyPermission struct {
 
 	FunctionCallPermission AccessKeyFunctionCallPermission
 	FullAccessPermission   struct{}
+}
+
+type fullAccessPermissionWrapper struct {
+	FunctionCall AccessKeyFunctionCallPermission `json:"FunctionCall"`
 }
 
 func NewFunctionCallPermission(allowance types.Balance, receiverID types.AccountID, methodNames []string) AccessKeyPermission {
@@ -36,12 +43,73 @@ func NewFunctionCallUnlimitedAllowancePermission(receiverID types.AccountID, met
 
 func NewFullAccessPermission() AccessKeyPermission {
 	return AccessKeyPermission{
-		Enum: borsh.Enum(0),
+		Enum: borsh.Enum(1),
 	}
 }
 
+func (a AccessKeyPermission) MarshalJSON() (b []byte, err error) {
+	if a.IsFullAccess() {
+		b = []byte(`"FullAccess"`)
+		return
+	}
+
+	var v fullAccessPermissionWrapper
+	v.FunctionCall = a.FunctionCallPermission
+
+	b, err = json.Marshal(&v)
+	return
+}
+
+func (a *AccessKeyPermission) UnmarshalJSON(b []byte) (err error) {
+	if len(b) > 0 && b[0] == '{' {
+		var permission fullAccessPermissionWrapper
+		if err = json.Unmarshal(b, &permission); err != nil {
+			return
+		}
+
+		*a = AccessKeyPermission{
+			Enum:                   borsh.Enum(0),
+			FunctionCallPermission: permission.FunctionCall,
+		}
+		return
+	}
+
+	var value string
+	if err = json.Unmarshal(b, &value); err != nil {
+		return
+	}
+
+	if value == "FullAccess" {
+		*a = NewFullAccessPermission()
+		return
+	}
+
+	err = fmt.Errorf("unknown permission '%s'", value)
+	return
+}
+
+func (a AccessKeyPermission) String() string {
+	var value string = "FullAccess"
+	if a.IsFunctionCall() {
+		value = a.FunctionCallPermission.String()
+	}
+	return fmt.Sprintf("AccessKeyPermission{%s}", value)
+}
+
+func (a *AccessKeyPermission) IsFunctionCall() bool {
+	return a.Enum == 0
+}
+
+func (a *AccessKeyPermission) IsFullAccess() bool {
+	return a.Enum == 1
+}
+
 type AccessKeyFunctionCallPermission struct {
-	Allowance   *types.Balance
-	ReceiverID  types.AccountID
-	MethodNames []string
+	Allowance   *types.Balance  `json:"allowance"`
+	ReceiverID  types.AccountID `json:"receiver_id"`
+	MethodNames []string        `json:"method_names"`
+}
+
+func (a AccessKeyFunctionCallPermission) String() string {
+	return fmt.Sprintf("AccessKeyFunctionCallPermission{Allowance=%v, ReceiverID=%v, MethodNames=%v}", a.Allowance, a.ReceiverID, a.MethodNames)
 }
